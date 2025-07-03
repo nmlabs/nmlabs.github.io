711ced5fd5eccb4074764bb0dfc495fe0e886a28
nandmlabs <nimariolabs@gmail.com>
2025-07-02 19:14:06 -0700

    timeline

--- app/page.tsx
+++ app/page.tsx
@@ -21,19 +21,36 @@ const jobs = [
   }
 ];
 
+const Timeline = () => {
+
+  return (
+    <ol className="relative border-s border-gray-300 ">
+      {
+        jobs.map(job=>(
+          <li  key={job.title} className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 dark:bg-gray-400"></div>
+            {/* <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time> */}
+            <h3 className="text-lg font-semibold">{job.title}</h3>
+            <p className="mb-4 text-base font-normal">{job.desc}</p>
+          </li>
+        ))
+      }
+    </ol>
+  );
+
+}
+
 
 export default function Home() {
   return (
     <>
       {/* <div className="max-w-4xl mx-auto bg-amber-100"> */}
-      <div className="max-w-4xl mx-auto ">
-        <main>
-          <div className="flex py-2">
-            <div className="mr-2 font-semibold">N&M Labs</div>
-            <div className="mr-2">services</div>
-            <div className="mr-2">about us</div>
-          </div>
-        </main>
+      <div className="mt-3 max-w-4xl mx-auto ">
+        <div className="flex py-4">
+          <div className="mr-2 font-semibold">N&M Labs</div>
+          <div className="mr-2">services</div>
+          <div className="mr-2">about-us</div>
+        </div>
         <div className="text-center p-20">
           <div className="flex justify-center items-center">
             <Blocks className="mb-1 mr-3 w-12 h-12" />
@@ -42,15 +59,10 @@ export default function Home() {
           <p className="">From complex workflows to real-time apps<br /> we turn your ideas into reliable systems. </p>
         </div>
         {/* <h1 className="text-2xl text-center">N&M Labs</h1> */}
-        <ul>
-          {jobs.map(job =>
-          (<li key={job.title} className="my-3">
-            <h3 className="text-lg font-semibold">{job.title}</h3>
-            <p className="p-2">{job.desc}</p>
-          </li>)
-          )
-          }
-        </ul>
+        <div>
+          <h2 className="text-3xl py-6 font-medium text-center">Our Most Recent Project</h2>
+        </div>
+        <Timeline />
       </div>
     </>
 
