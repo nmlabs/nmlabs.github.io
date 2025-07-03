66a69f56a991fdb8b7fc3f46bddfaf12b0c1bb70
nandmlabs <nimariolabs@gmail.com>
2025-07-03 00:43:18 -0700

    front page

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,8 +12,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>{children}</body>
+
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -22,13 +22,12 @@ const jobs = [
 ];
 
 const Timeline = () => {
-
   return (
     <ol className="relative border-s border-gray-300 ">
       {
-        jobs.map(job=>(
-          <li  key={job.title} className="mb-10 ms-4">
-            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 dark:bg-gray-400"></div>
+        jobs.map(job => (
+          <li key={job.title} className="mb-10 ms-4">
+            <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -start-1.5 dark:bg-gray-700"></div>
             {/* <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">February 2022</time> */}
             <h3 className="text-lg font-semibold">{job.title}</h3>
             <p className="mb-4 text-base font-normal">{job.desc}</p>
@@ -45,26 +44,60 @@ export default function Home() {
   return (
     <>
       {/* <div className="max-w-4xl mx-auto bg-amber-100"> */}
-      <div className="mt-3 max-w-4xl mx-auto ">
-        <div className="flex py-4">
+      <div className="text-center bg-blue-500 text-white">
+        <div className="flex py-4 pl-20 text-lg ">
           <div className="mr-2 font-semibold">N&M Labs</div>
           <div className="mr-2">services</div>
           <div className="mr-2">about-us</div>
         </div>
-        <div className="text-center p-20">
-          <div className="flex justify-center items-center">
+
+        <div className="p-30">
+          <div className="flex justify-center items-center ">
             <Blocks className="mb-1 mr-3 w-12 h-12" />
-            <h1 className="italic text-4xl py-6 font-medium text-center">We Build Smart Software</h1>
+            <h1 className="italic text-4xl py-6 font-semibold text-center">We Build Smart Software</h1>
           </div>
-          <p className="">From complex workflows to real-time apps<br /> we turn your ideas into reliable systems. </p>
+          <p className="text-lg">Simplifying complex workflows to real-time apps<br /> we turn your ideas into reliable systems. </p>
+
         </div>
+      </div>
+      <div className="mt-3 max-w-4xl mx-auto ">
+        {/*Title*/}
         {/* <h1 className="text-2xl text-center">N&M Labs</h1> */}
-        <div>
-          <h2 className="text-3xl py-6 font-medium text-center">Our Most Recent Project</h2>
+        <div className="mb-40 mt-20">
+          <h2 className="text-3xl font-medium pb-4 mb-6 text-center">From Boring Spreadsheets to <br /> a Beautiful Webapp</h2>
+          <div className="flex w-full mb-6">
+            <img className="w-1/2 object-cover pr-2" src={"/spreadsheet.png"} />
+            <img className="w-1/2 object-cover pl-2" src={"/webapp.png"} />
+          </div>
+          <div>
+            <div className="w-full overflow-hidden">
+              <video
+                src="/demo.mov"
+                autoPlay
+                muted
+                className="w-full h-full object-cover"
+              />
+            </div>
+
+            {/* <video autoPlay muted >
+              <source src="/demo.mov" />
+            </video> */}
+          </div>
+          <div className="mb-8 mt-8 ">
+            <h3 className="text-2xl pb-2">Dayton Sheets</h3>
+            <p className="pb-2">
+              We converted their laborious process from entering seller data in a buyer sheet into a streamlined full stack web app.
+            </p>
+            <p>
+              The vision for the project was to have a master sheet with products that sellers could enter their quotes and they would be grouped and displayed. This setup of private sellers, a master sheet where any changes would reflect seller sheets and providing real time updates.
+            </p>
+          </div>
         </div>
+        <div>
+        </div>
+        <h2 className="text-3xl font-medium pb-4 mb-6 text-center">Past Projects</h2>
         <Timeline />
       </div>
     </>
-
   );
 }
